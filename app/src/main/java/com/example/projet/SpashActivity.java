edc2bd703f0838beb49c105c8ee443c006f862cc
hassinegreguiche13 <hassinegreguiche13@gmail.com>
2025-04-15 14:46:48 +0100

    Dernières modifications

--- app/src/main/java/com/example/projet/SpashActivity.java
+++ app/src/main/java/com/example/projet/SpashActivity.java
@@ -1,5 +1,6 @@
 package com.example.projet;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.projet.databinding.ActivitySpashBinding;
+
 public class SpashActivity extends AppCompatActivity {
 
+    private ActivitySpashBinding binding ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_spash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding=ActivitySpashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.set
+
     }
 }
